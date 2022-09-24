078d28f66199a7fffe4eafb053f8386a47a5976a
Florian Zand <florianzand@me.com>
2022-09-25 01:36:08 +0200

    Update NSContentView.swift

--- Sources/FZExtensions/Classes/NSConfiguration/NSContentView.swift
+++ Sources/FZExtensions/Classes/NSConfiguration/NSContentView.swift
@@ -11,6 +11,13 @@ import AppKit
 public protocol NSContentView {
     var configuration: NSContentConfiguration { get set }
     func supports(_ configuration: NSContentConfiguration) -> Bool
+    func sizeThatFits(_ size: CGSize) -> CGSize
+}
+
+public extension NSContentView where Self: NSView {
+     func sizeThatFits(_ size: CGSize) -> CGSize {
+        return self.frame.size
+    }
 }
 
 #endif
